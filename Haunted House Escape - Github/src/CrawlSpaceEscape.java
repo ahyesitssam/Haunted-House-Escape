b2007a2dcce6c80c271416564fbd2da06659c742
elenal2268 <levinee7@miamioh.edu>
2025-05-01 00:21:25 -0400

    Made more javadoc progress--not complete yet

--- Haunted House Escape - Github/src/CrawlSpaceEscape.java
+++ Haunted House Escape - Github/src/CrawlSpaceEscape.java
@@ -1,34 +1,46 @@
 import java.util.Scanner;
 
 /**
- * Class: CrawlSpaceEscape  
- * Group: C  
- * Version: 1.1  
- * Course: CSE201 E  
- * Written: April 29, 2025  
+ * Class: CrawlSpaceEscape
+ * Group: C
+ * Version: 1.1
+ * Course: CSE201 E
+ * Written: April 29, 2025
  * 
- * Purpose: This class allows the player to navigate a crawl space by making 
+ * Purpose: This class allows the player to navigate a crawl space by making
  * correct directional choices. If the user chooses wrong, they must restart.
  */
 public class CrawlSpaceEscape extends Outside {
     private Inventory inventory;
     private Scanner scanner = new Scanner(System.in);
-    private String[] correctPath = {"left", "straight", "right"};
+    private String[] correctPath = { "left", "straight", "right" };
 
+    /**
+     * Constructor: initializes CrawlSpaceEscape
+     * @param inventory initializes user inventory
+     **/
     public CrawlSpaceEscape(Inventory inventory) {
         this.inventory = inventory;
     }
 
+    /**
+     * Method used to enter the crawlspace, calling chooseDirection() and 
+     * escapeCrawlSpace() in order for user to make their way through it,
+     * eventually reaching the end.
+     **/
     @Override
     public void enter() {
         System.out.println("You squeeze into the narrow path and make your way... until you find" +
-        " three tunnles branching off of your route.");
+                " a series of three tunnles branching off of your route.");
         keyPress();
         chooseDirection();
         escapeCrawlSpace();
     }
 
-    // Handles navigation through the crawlspace
+    /**
+     * Method used to handle right and wrong directions taken by user
+     * after calling getUserDirection()
+     **/
     private void chooseDirection() {
         int index = 0;
         while (index < correctPath.length) {
@@ -44,9 +56,13 @@ public class CrawlSpaceEscape extends Outside {
             }
         }
     }
-    
 
-    // Presents direction choices to the user and returns their chosen direction
+    /**
+     * Method used to handle navigation through crawlspace by presenting
+     * user with different directions to take. If user chooses invalid 
+     * option, they're prompted to choose again.
+     * @return string representation of direction user chooses.
+     **/
     private String getUserDirection() {
         int choice = 0;
         while (choice < 1 || choice > 3) {
@@ -57,31 +73,37 @@ public class CrawlSpaceEscape extends Outside {
             System.out.print("Choose a number: ");
             if (scanner.hasNextInt()) {
                 choice = scanner.nextInt();
-                scanner.nextLine(); 
+                scanner.nextLine();
             } else {
-                scanner.nextLine(); 
+                scanner.nextLine();
                 System.out.println("Invalid input. Please enter a number between 1 and 3.");
             }
         }
-    
+
         switch (choice) {
-            case 1: return "left";
-            case 2: return "right";
-            case 3: return "straight";
-            default: return ""; 
+            case 1:
+                return "left";
+            case 2:
+                return "right";
+            case 3:
+                return "straight";
+            default:
+                return "";
         }
     }
-    
 
-    // Let's user finally escape once correctly choosing all correct direction
+    /**
+     * Method for when user reaches the end of crawlspace, narrating the 
+     * end of game and exits game.
+     **/
     private void escapeCrawlSpace() {
         System.out.println("You finally reach a hatch. This must be the end... " +
-        " You push it open and feel a rush of cold air against your face...");
+                " You push it open and feel a rush of cold air against your face...");
         keyPress();
         System.out.println("You tremble to your knees as the light of day shrinks" +
-        " your pupils and droplets of rain patter your skin.\nStumbling out the dark," +
-        " unsettling pathway, you take one look back at the foreboding house" +
-        " and finally scurry away, forever changed by the haunting encounter.");
+                " your pupils and droplets of rain patter your skin.\nStumbling out the dark," +
+                " unsettling pathway, you take one look back at the foreboding house" +
+                " and finally scurry away, forever changed by the haunting encounter.");
         System.out.println("Congratulations! You escaped the haunted house!");
         keyPress();
         System.exit(0); // Ends game
--- Haunted House Escape - Github/src/DiningRoom.java
+++ Haunted House Escape - Github/src/DiningRoom.java
@@ -2,33 +2,47 @@ public class DiningRoom extends Room {
 
     public DiningRoom(Inventory inventory) {
         super(inventory);
-        this.description="Spiderwebs cover the chairs and drawers are loaded with silverware and fine China sets.";  
+        this.description = "Spiderwebs cover the chairs and drawers are loaded with silverware and fine China sets.";
     }
 
+    /**
+     * Method that presents user with different locations to choose from.
+     * @return int that user chooses, corrosponding to one of the locations.
+     **/
     @Override
-	public int roomChoice() {
-		int choice = 0;
-		while (choice <= 0 || choice > 5) {
-			System.out.println("-> What will you do?");
-			System.out.println("1. Living room");
-			System.out.println("2. Master bedroom");
-			System.out.println("3. Dining room");
-			System.out.println("4. Upstairs");
+    public int roomChoice() {
+        int choice = 0;
+        while (choice <= 0 || choice > 5) {
+            System.out.println("-> What will you do?");
+            System.out.println("1. Living room");
+            System.out.println("2. Master bedroom");
+            System.out.println("3. Dining room");
+            System.out.println("4. Upstairs");
             System.out.println("5. View painting on wall");
-			System.out.print("Choose a number: ");
-			choice = numberChoice(5);
-		}	
-		return choice;
-	}
+            System.out.print("Choose a number: ");
+            choice = numberChoice(5);
+        }
+        return choice;
+    }
 
+    /**
+     * Method used to present painting on the wall, introducing its code. 
+     **/
     @Override
     public void voidInteractionChoice() {
-		System.out.println("You're lured to the painting on the wall, and gently dust its surface with your fingertips." 
-        + " Underneath the debris lies a sad, brooding man with his hands clamped into fists and color drained from his"
-        + " face. You go to dust the surface even more, and are shocked to find a faint sentence written in the bottom"
-        + " corner: 'The second bell tolls twice, the first serpent coils thrice, and the final step echoes once in shadow.'\n"); // (code 3 2 1)
+        System.out.println("You're lured to the painting on the wall, and gently dust its surface with your fingertips."
+                + " Underneath the debris lies a sad, brooding man with his hands clamped into fists and color drained from his"
+                + " face. You go to dust the surface even more, and are shocked to find a faint sentence written in the bottom"
+                + " corner: 'The second bell tolls twice, the first serpent coils thrice, and the final step echoes once in shadow.'\n"); 
     }
 
+    /**
+     * Method used to get the maximum choice the user can choose within 
+     * roomChoice(), which will be the only option introducing an 
+     * interaction within the room. This triggers voidInteractionChoice() 
+     * in this class once called in Room class.
+     * @return maximum choice: 5
+     **/
     @Override
     public int getChoiceMax() {
         return 5;
